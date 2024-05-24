70000d81d62dc5ce2dd9f65ea735e814d0556c30
Nelson Barros <nelson.barros@al.infnet.edu.br>
2024-05-24 19:17:48 -0300

    modificações 24/05 import sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
 INSERT INTO tb_usuario (id, nome, email, senha)  VALUES ('2D1EBC5B7D2741979CF0E84451C5BBB1', 'pedro', 'pedro@gmail.com', '123244')
 
-INSERT INTO tb_livro (id, titulo, autor, genero, isbn, sinopse)  VALUES ('b4cd38cd-951e-454c-8b95-240f0a01d8f7', , 'Titulo livro', 'Autor livro', 'Aventura', '12345', 'Livro mto legal')
+INSERT INTO tb_livro (id, titulo, autor, genero, isbn, sinopse)  VALUES ('b4cd38cd-951e-454c-8b95-240f0a01d8f7' , 'Titulo livro', 'Autor livro', 'Aventura', '12345', 'Livro mto legal')
