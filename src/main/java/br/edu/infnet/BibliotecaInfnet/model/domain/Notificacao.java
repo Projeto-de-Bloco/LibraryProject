ae6b38f571479cf9c4b69ac5f9df33e6c0816cdb
bruno.calves <brunocister@gmail.com>
2024-06-07 19:06:35 -0300

    mudancas notificacao

--- src/main/java/br/edu/infnet/BibliotecaInfnet/model/domain/Notificacao.java
+++ src/main/java/br/edu/infnet/BibliotecaInfnet/model/domain/Notificacao.java
@@ -1,7 +1,6 @@
 package br.edu.infnet.BibliotecaInfnet.model.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,12 +18,24 @@ import java.util.UUID;
 
 public class Notificacao {
 
+        @Id
+        @GeneratedValue(strategy = GenerationType.UUID)
+        private UUID id;
         private UUID userId;
         private UUID donoId;
         private String mensagem;
         private String nomeLivro;
         private LocalDateTime dataNotificacao;
         private LocalDateTime dataLimite;
+
+        public Notificacao(UUID locatarioLivro, UUID donoLivro, String mensagem, String tituloLivro, LocalDateTime now, LocalDateTime dataVencimento) {
+                this.userId = locatarioLivro;
+                this.donoId = donoLivro;
+                this.nomeLivro = tituloLivro;
+                this.mensagem = mensagem;
+                this.dataNotificacao = now;
+                this.dataLimite = dataVencimento;
+        }
 }
 
 
